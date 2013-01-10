5ec27fd81b23faf23b4c9d97a4c986e762a66687
spencerg <spencerg@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-01-10 00:02:24 +0000

    asNumericChar
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ecdat@10 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/asNumericDF.R
+++ pkg/R/asNumericDF.R
@@ -1,20 +1,21 @@
-asNumericDF <- function(x, keep=function(x)any(!is.na(x)),
-                        orderBy){
-  decrypt <- function(x){
+asNumericChar <- function(x){
 # Delete commas (thousand separators) and footnote references
     x1 <- gsub(',', '', x)
     x2 <- strsplit(x1, ' ')
     x. <- sapply(x2, '[', 1)
     x.[x1==''] <- NA
     as.numeric(x.)
-  }
+}
+
+asNumericDF <- function(x, keep=function(x)any(!is.na(x)),
+                        orderBy){
 #
   if(is.function(keep)){
       Keep <- sapply(x, keep)
   } else Keep <- 1:ncol(x)
   x. <- x[, Keep]
 #
-  X <- lapply(x., decrypt)
+  X <- lapply(x., asNumericChar)
 #
   if(missing(orderBy)){
       orderBy <- 1:length(X)
