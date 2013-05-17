78be593ea367ebe055953c8c7689d266dd313287
spencerg <spencerg@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-05-17 19:24:46 +0000

    grepNonEnglish: add "\n" to standardCharacters
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ecdat@79 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/grepNonEnglish.R
+++ pkg/R/grepNonEnglish.R
@@ -1,14 +1,23 @@
-grepNonEnglish <- function(x, value=TRUE,
+grepNonEnglish <- function(x, value=FALSE,
    standardCharacters=c(letters, LETTERS, ' ','.', ',',
-       '\"', "\'", '-', '(', ')', '[', ']') ) {
-    x. <- strsplit(x, '')
-    x.in.sC <- function(y){
-        x0 <- which(!(y %in% standardCharacters))
-        if(length(x0)<1) 0 else x0[1]
-    }
-    x2 <- sapply(x., x.in.sC)
-    x1 <- which(x2>0)
-    if(value){
-        return(x[x1])
-    } else return(x1)
+       '\"', "\'", '-', '(', ')', '[', ']', '\n') ) {
+##
+## 1.  split single characters
+##
+  x. <- strsplit(x, '')
+##
+## 2.  find !standardCharacters
+##
+  x.in.sC <- function(y){
+      x0 <- which(!(y %in% standardCharacters))
+      if(length(x0)<1) 0 else x0[1]
+  }
+  x2 <- sapply(x., x.in.sC)
+##
+## 3.  Return indices or value
+##
+  x1 <- which(x2>0)
+  if(value){
+      return(x[x1])
+  } else return(x1)
 }
