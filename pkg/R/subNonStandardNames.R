a7ffceec544c89885b406edc6230267043967df5
spencerg <spencerg@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-05-20 19:12:38 +0000

    subNonStandardNames
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ecdat@91 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/subNonStandardNames.R
@@ -0,0 +1,21 @@
+subNonStandardNames <- function(x,
+   standardCharacters=c(letters, LETTERS, ' ','.', ',',
+      '\"', "\'", '-', '(', ')', '[', ']', '\n'),
+   replacement='_', nonStandardNames=nonEnglishNames, ...) {
+##
+## 1.  x. <- subNonStandardCharacters(x, ...)
+##
+  x. <- subNonStandardCharacters(x, standardCharacters, replacement, ...)
+##
+## 2.  loop over rows of nonStandardNames
+##
+  nSN <- nrow(nonStandardNames)
+  for(iSN in seq(length=nSN)){
+      x. <- gsub(nonStandardNames[iSN, 1], nonStandardNames[iSN, 2],
+                 x.)
+  }
+##
+## 3.  done
+##
+  x.
+}
