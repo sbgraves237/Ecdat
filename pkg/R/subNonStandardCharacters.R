512c482d499c8968e7eaecd01744b74867de6bb1
spencerg <spencerg@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-05-21 19:01:28 +0000

    subNonStandardCharacters gsubList
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ecdat@96 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/subNonStandardCharacters.R
+++ pkg/R/subNonStandardCharacters.R
@@ -1,14 +1,31 @@
 subNonStandardCharacters <- function(x,
    standardCharacters=c(letters, LETTERS, ' ','.', ',', 0:9,
       '\"', "\'", '-', '_', '(', ')', '[', ']', '\n'),
-   replacement='_', ... ) {
+   replacement='_',
+   gsubList=list(list(pattern='\\\\\\\\|\\\\', replacement='\"')),
+   ... ) {
 ##
-## 1.  x. <- strsplit(x, "", ...)
+## 1.  gsubList
 ##
-  nx <- length(x)
-  x. <- strsplit(x, "", ...)
+  xo <- x
+  ng <- length(gsubList)
+  for(ig in seq(length=ng)){
+      gsLi <- gsubList[[ig]]
+      if(!is.list(gsLi)){
+          print(deparse(substitute(gsubList)))
+          cat(ig, '\n')
+          print(gsLi)
+          stop('gsubList[[', ig, ']] is NOT a list')
+      }
+      xo <- gsub(gsLi$pattern, gsLi$replacement, xo)
+  }
 ##
-## 2.  check each and modify as needed
+## 2.  x. <- strsplit(x, "", ...)
+##
+  nx <- length(xo)
+  x. <- strsplit(xo, "", ...)
+##
+## 3.  check each and modify as needed
 ##
   for(ix in seq(length=nx)){
       gi <- which(!(x.[[ix]] %in% standardCharacters))
@@ -18,8 +35,11 @@ subNonStandardCharacters <- function(x,
           xi1 <- paste(x.[[ix]][seq(length=gi.[1]-1)], collapse='')
           xie <- paste(x.[[ix]][seq(from=gi.[2]+1, length=ni-gi.[2])],
                        collapse='')
-          x[ix] <- paste(xi1, replacement, xie, sep="")
+          xo[ix] <- paste(xi1, replacement, xie, sep="")
       }
   }
-  x
+##
+## 4.  Done
+##
+  xo
 }
