9d61d6dd6e93303e3c32ea25d1bc64bd7f39612b
spencerg <spencerg@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-09-18 21:46:34 +0000

    CookPVI
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ecdat@116 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/readCookPVI.R
@@ -0,0 +1,60 @@
+readCookPVI <- function(url=
+"http://en.wikipedia.org/wiki/Cook_Partisan_Voting_Index",
+    stateAbbreviations=USstateAbbreviations){
+##
+## 1.  download content
+##
+  library(RCurl)
+  url. <- getURL(url)
+##
+## 2.  readHTMLTable
+##
+  library(XML)
+  Wikitbls <- readHTMLTable(url., stringsAsFactors=FALSE)
+##
+## 3.  Find House and Senate tables
+##
+  len <- sapply(Wikitbls, length)
+  Wikitbs <- Wikitbls[len>0]
+  ns <- sapply(Wikitbs, dim)
+  house <- which((434<ns[1,]) & (ns[1,]<450))
+  if((nh <- length(house))!=1){
+      stop('URL source changed: Found ', nh,
+           ' tables with between 434 and 450 rows.  oops.')
+  }
+  House <- Wikitbs[[house]]
+  senate <- which(ns[1,]==50)
+  if((nsen <- length(senate)) != 1){
+      stop('URL source changed:  Found ', nsen,
+           ' tables with 50 rows.  oops.')
+  }
+  Senate <- Wikitbs[[senate]]
+##
+## 4.  Parse PVI columns
+##
+  pvi <- function(x){
+      x. <- strsplit(x, ' !')
+      PVIn <- sapply(x., '[', 1)
+      PVInum <- as.numeric(PVIn)
+      PVIchar <- sapply(x., '[', 2)
+      list(PVInum=PVInum, PVIchar=PVIchar)
+  }
+  House. <- cbind(House[c('State', 'District')],
+                  pvi(House[['PVI']]),
+                  stringsAsFactors=FALSE)
+  House.$PartyOfRepresentative <- factor(House[['Party of\nRepresentative']])
+#
+  Senate. <- cbind(Senate['State'], pvi(Senate[['PVI']]),
+                   stringsAsFactors=FALSE)
+  Senate.$PartyOfGovernor <- factor(Senate[['Party of\nGovernor']])
+  Senate.$PartyInSenate <- factor(Senate[['Party\nin Senate']])
+  houseBal <- pvi(Senate[['House\nbalance']])
+  Senate.$houseBalanceNum <- houseBal[[1]]/10
+  Senate.$houseBalanceChar <- houseBal[[2]]
+##
+## 8.  Done
+##
+  list(House=House., Senate=Senate.)
+
+}
+
