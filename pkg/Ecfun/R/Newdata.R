bdf7cc6f8a3f7f73c00a881f8902e6cce03e51d7
spencerg <spencerg@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-04-22 03:07:28 +0000

    better Newdata
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ecdat@333 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Ecfun/R/Newdata.R
+++ pkg/Ecfun/R/Newdata.R
@@ -1,69 +1,132 @@
-Newdata <- function(data, x, n=31, na.rm=TRUE){
+Newdata <- function(data, x, n, na.rm=TRUE){
 ##
-## 1.  check data, x
+## 1.  check data
 ##
-  if(length(x) != 1)
-    stop('length(x) must be 1; is ', length(x))
-  if(is.numeric(x))x <- colnames(data)[x]
-  vars <- colnames(data)
-  if(!(x %in% vars))
-    stop('x = ', x, ' not in colnames(data) = ', 
-         paste(vars, collapse=', '))
+  DF <- inherits(data, 'data.frame')
+  Dat <- as.data.frame(data)
+  vars <- names(Dat)
 ##
-## 2.  canbeNumeric(x) 
-##
-  x. <- data[, x]
-  if(canbeNumeric(x.)){
-    xn <- as.numeric(x.)
-    xrng <- range(xn)
-    xNew <- seq(xrng[1], xrng[2], length=n)
-    attributes(xNew) <- attributes(x.)
+## 2.  check x
+##  
+  if(missing(x)){
+    x <- names(Dat)
   } else {
-    xlvls <- levels(x.)
-    if(!is.null(xlvls)){
-      n <- length(xlvls)
-      xNew <- x.[rep(1, n)]
-      for(i in 1:n){
-        xNew[i] <- xlvls[i]
-      } 
-    } else xNew <- NULL
-  }
-##
-## 3.  is.null(xNew) fix
-## 
-  if(is.null(xNew)) {
-    xNew <- sort(unique(x.))
-    n <- length(xNew)
-  }
-##
-## 4.  create newDat and set x
-##
-  newDat <- data[rep(1, n), , drop=FALSE]
-  rownames(newDat) <- NULL
-  newDat[, x] <- xNew 
-##
-## 5.  otherVars <- vars[!(vars == x)]
-##
-  otherVars <- vars[!(vars==x)]
-##
-## 6.  Replace otherVars as desired
-##
-  for(x2 in otherVars){
-    x2. <- data[, x2]
-    if(canbeNumeric(x2.)){
-      x2m <- median(as.numeric(x2.))
-      attributes(x2m) <- attributes(x2.)
-      newDat[, x2] <- x2m
+    if(is.numeric(x)){
+      x <- vars[x]
     } else {
-      x2t <- table(x2.)
-      x2s <- sort(x2t, decreasing=TRUE)
-      x2new <- names(x2s[1])
-      sel <- which(data[, x2] == x2new)
-      newDat[, x2] <- data[sel[1], x2]
+      oops <- which(!(x %in% vars))
+      if(length(oops)>0){
+        msg0 <- paste('Some elements of x are not', 
+                  'in colnames(data):')
+        if(length(oops)>4){
+          msg1 <- paste(msg0, paste(oops[1:4], collapse=', '), 
+                        '...')
+        } else msg1 <- paste(msg0, paste(oops, collapse=', '))
+        msg2 <- paste(msg1, 'not in colnames(data) =')
+        if(length(vars)>4){
+          msg3 <- paste(msg2, paste(vars[1:4], collapse=', '), 
+                        '...')
+        } else {
+          msg3 <- paste(msg2, paste(vars, collapse=', '))
+        }
+        stop(msg3)
+      }
     }
   }
 ##
-## 8.  done 
+## 3.  check n
+##
+  nx <- length(x)
+  if(missing(n)){
+    n <- rep(2, nx) 
+  } else {
+    if(length(n)<1) {
+      n <- rep(2, nx) 
+    } else {
+      if(length(n)<2){
+        if(is.na(n)){
+          n <- rep(2, nx)
+        } else {
+          n <- rep(n, nx)
+        }
+      } else {
+        if(length(n) != length(x)){
+          stop('length(n) = ', length(n), 
+               ' != length(x) = ', length(x))
+        }
+      }
+    }
+  }
+  if(is.null(names(n))){
+    names(n) <- x
+  } else {
+    oopn <- which(!(names(n) %in% x))
+    if(length(oopn)>0){
+      stop('names(n) = ', names(n)[oopn[1]], 
+           ' not matched in x; x[1] =', 
+           x[1])
+    }
+  }
+##
+## 4.  loop over vars  
+##
+  Lat <- as.list(Dat)
+  for(v in vars){
+    if(v %in% x){
+      if(n[v]<1){
+        Lat[[v]] <- NULL 
+      } else {
+        v. <- data[, v]
+        if(canbeNumeric(v.)){
+          vn <- as.numeric(v.)
+          if(n[v]<2){
+            vm <- median(vn)
+            attributes(vm) <- attributes(v.)
+            Lat[[v]] <- vm 
+          } else {
+            vrng <- range(vn)
+            vNew <- seq(vrng[1], vrng[2], length=n[v])
+            attributes(vNew) <- attributes(v.)
+            Lat[[v]] <- vNew 
+          }
+        } else {
+          vt <- sort(table(v.), TRUE)
+          if(length(vt) <= n[v]){
+            Vt <- names(vt)
+          } else {
+            Vt <- names(vt)[1:n[v]]
+          }
+          iv <- 1:length(Vt)
+          Vt. <- v.[iv]
+          for(i. in iv){
+            jv <- which(v. == Vt[i.])
+            Vt.[i.] <- v.[jv[1]]
+          }
+          Lat[[v]] <- Vt.
+        }
+      }
+    } else {
+      v. <- data[, v]
+      if(canbeNumeric(v.)){
+        vn <- as.numeric(v.)
+        vm <- median(vn)
+        attributes(vm) <- attributes(v.)
+        Lat[[v]] <- vm 
+      } else {
+        vt <- sort(table(v.), TRUE)
+        Vt <- names(vt)[1]
+        jv <- which(v. == Vt)
+        Lat[[v]] <- v.[jv[1]]
+      }
+    }
+  }
+##
+## 5.  create newDat 
+##
+  Lat$stringsAsFactors <- FALSE 
+  newDat <- do.call(expand.grid, Lat)
+##
+## 6.  done 
 ##  
   newDat
 }
