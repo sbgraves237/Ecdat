6ec64b9a82c7da5d1c75dbfb530da9f825716b83
spencerg <spencerg@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-07-21 20:38:25 +0000

    asNumericDF w col2names
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ecdat@363 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Ecfun/R/asNumericDF.R
+++ pkg/Ecfun/R/asNumericDF.R
@@ -49,7 +49,9 @@ asNumericDF <- function(x, keep=function(x)any(!is.na(x)),
         POSIX=NULL, format.){
 ##
 ## 1.  Copy x
-##  
+##
+  Trace <- FALSE
+  if(Trace)cat('debug asNumericDF\n')
   X <- as.data.frame(x)
 ##  
 ## 2.  Confirm that ignore, factors, Dates, and POSIX
@@ -57,34 +59,76 @@ asNumericDF <- function(x, keep=function(x)any(!is.na(x)),
 ##
   k <- ncol(x)
   Names <- colnames(x)
-#   check for Names in referenceList    
-  if(is.numeric(ignore)){
-    if(any(ignore<1)){
-      stop('numeric ignore < 1')
+#   check for Names in referenceList   
+  cols2names <- function(cols=ignore){
+    if(length(cols)<1)return(character(0))
+    msg <- deparse(substitute(cols))
+    if(is.numeric(cols)){
+      if(any(cols<1)){
+        stop(msg, ' is numeric < 1')
+      } 
+      if(any(cols>k)){
+        stop(msg, ' is numeric > ncol(x)')
+      }
+      colNames <- Names[cols]
+    } else {
+      if(is.logical(cols)){
+        if((lc <- length(cols))<k){
+          stop(msg, ' is logical with length = ', 
+              lc, ' < ncol(x) = ', k)
+        }
+        colNames <- Names[cols]
+      } else {
+        if(!is.character(cols)){
+          stop(msg, ' is class = ', class(cols), 
+               '; not allowed')
+        }
+        if(length(igoops <- which(!(cols %in% Names)))>0){
+          stop(msg, ' = ', cols[igoops[1]], 
+             ' not in names(x) = ', 
+             paste(Names, collapse=', ') )
+        }
+        colNames <- cols
+      }
+      if(length(unique(colNames))<length(colNames)){
+        stop(msg, ' contains duplicates')
+      }
     }
-    if(any(ignore>k)){
-      stop('ignore numeric > ncol(x)')
-    }
-    ignore <- colnames(x)[ignore]
-  } else {
-    if(length(igoops <- which(!(ignore %in% Names)))>0){
-      stop('ignore = ', ignore[igoops[1]], 
-           ' not in names(x) = ', 
-           paste(Names, collapse=', ') )
-    }
+    colNames    
   }
-# skip tests of factors, Dates, and POSIX
-# ; implement later
+  Ignore <- cols2names(ignore)
+  Factors <- cols2names(factors)
+  DATES <- cols2names(Dates)
+  Posix <- cols2names(POSIX)
+##
+## test for intersection 
+##  
+  if(length(IFoops <- intersect(Ignore, Factors)>0)){
+    stop(IFoops[1], ' is in both ignore and factors')
+  }
+  IgFa <- c(Ignore, Factors)
+  if(length(IFDoops <- intersect(DATES, IgFa))>0){
+    stop(IFDoops[1], ' is in Dates and either ignore', 
+         ' or factors')
+  }
+  IgFaDa <- c(IgFa, DATES)
+  if(length(IFDPoops <- intersect(Posix, IgFaDa))>0){
+    stop(IFDPoops[1], ' is in POSIX and one of ignore', 
+         ', factors, and Dates')
+  }
+  dontConvert <- c(IgFaDa, Posix)
 ##  
 ## 3.  Convert factors, Dates, and POSIX 
 ##
-  for(f in factors){
+  if(Trace)cat('factors\n')
+  for(f in Factors){
     X[, f] <- factor(x[, f])
   }
-  for(d in Dates){
+  if(Trace)cat('Dates\n')
+  for(d in DATES){
     xd <- x[, d]
     if(is.factor(xd))xd <- as.character(xd)
-    dd <- try(as.Date(xd, format.))
+    dd <- try(as.Date(xd, format=format.))
     if(is(dd, 'try-error')){
       dd1 <- try(as.Date(xd, '%m-%d-%Y'))
       dd2 <- try(as.Date(xd, '%m/%d/%Y'))
@@ -137,16 +181,20 @@ asNumericDF <- function(x, keep=function(x)any(!is.na(x)),
       }
     }
   }
-  for(p in POSIX){
+  if(Trace)cat('POSIX\n')
+  for(p in Posix){
+    xp <- x[, p]
+    xpNA <- (xp %in% c('NA', 'NULL'))
+    xp[xpNA] <- NA
     if(missing(format.)){
-      pp <- try(as.POSIXct(x[, p]))
+      pp <- try(as.POSIXct(xp))
       if(is(pp, 'try-error')){
         msgP <- paste0('Failed to convert POSIX ', 
                       d, ' = ', x[1, d], ', ...')
         stop(msgP)
       } else X[, p] <- pp 
     } else {
-      pp <- try(as.POSIXct(x[, p], format.))
+      pp <- try(as.POSIXct(xp, format=format.))
       if(is(pp, 'try-error')){
         msgP <- paste0('Failed to convert POSIX ', 
                        d, ' = ', x[1, d], ', ...')
@@ -158,11 +206,7 @@ asNumericDF <- function(x, keep=function(x)any(!is.na(x)),
 ## 4.  Apply asNumericChar to all columns 
 ##     not in ignore, factors, Dates, or POSIX.  
 ##
-  dontConvert <- union(ignore, union(factors, 
-                            union(Dates, POSIX)))
-  if(is.numeric(dontConvert)){
-    dontConvert <- names(x)[dontConvert]
-  }
+  if(Trace)cat('dontConvert\n')
   notNum <- (Names %in% dontConvert)
   numCols <- Names[!notNum]
   for(n in numCols){
@@ -189,6 +233,7 @@ asNumericDF <- function(x, keep=function(x)any(!is.na(x)),
 ##
 ## 5.  Keep columns specified by keep.  
 ##
+  if(Trace)cat('keep\n')
   kp <- rep(FALSE, k)
   names(kp) <- Names
   kp[notNum] <- TRUE
