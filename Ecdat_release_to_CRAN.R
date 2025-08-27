96cc1cc48e77f281c0654e6c6a3edfb897708a2f
sbgraves237 <spencer.graves@effectivedefense.org>
2025-08-27 12:29:48 -0500

    modify the release prce

--- Ecdat_release_to_CRAN.R
+++ Ecdat_release_to_CRAN.R
@@ -1,4 +1,4 @@
-# File > "Open Project..." > [parent of Package file]
+# File > "Open Project..." > [Package folder]
 
 # https://r-pkgs.org/release.html
 
@@ -7,36 +7,64 @@
 #20.3.  The submission process
 #       Store submission comments in cran-comments.md
 #20.3.1.  Test environments
+
 # devtools::check_win_*()
 dir()
 
-PkgName <- "Ecdat"
-PkgRegExpr <- paste0('^', PkgName, '$')
-(Pkg <- dir(pattern=PkgRegExpr))
+devtools::check()
+devtools::check_win_devel()
+devtools::check_win_release()
+devtools::check_win_oldrelease()
 
-devtools::check_win_devel(Pkg)
-devtools::check_win_release(Pkg)
-devtools::check_win_oldrelease(Pkg)
+# devtools::check_rhub()
+#This function is deprecated and defunct since rhub v2.
+#Please see `?rhubv2` on transitioning to the new rhub functions.
 
-devtools::check_rhub(Pkg)
+#rhubv2::rhub_setup()
+#Error in loadNamespace(x) : there is no package called ‘rhubv2’
+#devtools::rhub_setup()
+#Error: 'rhub_setup' is not an exported object from 'namespace:devtools'
+
+#Notes:
+#  • The workflow file must be added to the default branch of the GitHub repository.
+#• GitHub actions must be enabled for the repository. They are disabled for forked
+#repositories by default.
+
+#Next steps:
+#  • Add the workflow file to git using `git add <filename>`.
+#• Commit it to git using `git commit` (if not committed already).
+#• Push the commit to GitHub using `git push` (if not pushed already).
+#• Call rhub::rhub_doctor() to check that you have set up R-hub correctly.
+#• Call rhub::rhub_check() to check your package.
+
+#rhub::rhub_doctor()
+# ... WOOT! You are ready to run rhub::rhub_check() on this package.
+#rhub::rhub_check()
+# 2024-11-10: This produced a list of 26 different platforms
+# with no obvious indication of a recommended default other 
+# than 0 or 1:26. I chose the former. 
 
 #20.3.3.  Reverse dependencies
 
 # install.packages("revdepcheck")
 #devtools::install_github('r-lib/revdepcheck')
 
-revdepcheck::revdep_reset(Pkg)
+revdepcheck::revdep_reset()
 
-revdepcheck::revdep_check(Pkg, num_workers = 4)
+revdepcheck::revdep_check(num_workers = 4)
+
+# local on parent to package: 
+# R CMD build Ecdat 
+# R CMD check --as-cran Ecdat_*.tar.gz
 
 #20.4.  Update README.md and NEWS.md 
 
 #20.5.  Submit to CRAN
 
 library(devtools)
-spell_check(Pkg)
+spell_check()
 
-release(Pkg)
+release()
 
 #20.6.  Update the version number for the future 
 #20.7.  Publicise
